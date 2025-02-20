c9b305424554989f43b08c27a2e7057c1a15c0b2
Mathias Dahlström <mathias.dahlstrom@gmail.com>
2025-02-20 10:08:46 +0100

    feat: renamed resource and cleaned up exported entries

--- lib/aks/vault-access.d.ts
+++
@@ -1,8 +0,0 @@
-import * as pulumi from '@pulumi/pulumi';
-export declare function setupVaultAccess(
-  clusterConfig: pulumi.Output<string>,
-): {
-  serviceAccount: pulumi.Output<string>;
-  namespace: pulumi.Output<string>;
-};
-//# sourceMappingURL=vault-access.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,10 @@
-export * as CoreNetwork from './network/core-network.js';
-export * as CloudflareConnector from './cloudflare/connector.js';
 export * as Cloudflared from './cloudflare/cloudflared.js';
-export * as CloudflareNetwork from './network/cloudflare.js';
 export * as HubDNSResolver from './dns/hub-dns-resolver';
 export * as HubDNS from './dns/hub-dns';
 export * as SpokeDNS from './dns/spoke-dns';
-export * as AzureRoles from './rbac/roles';
 export * as Vault from './vault';
-export * as ManagedAks from './aks/managed-cluster';
+export * as AzureKubernetes from './aks/managed-cluster';
 export * as AzureKeyVault from './kms/azure-key-vault';
+export * as AzureRoles from './rbac/roles';
+export * as AzureVnet from './network/core-network.js';
 //# sourceMappingURL=index.d.ts.map
