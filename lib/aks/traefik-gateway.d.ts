415c137288630136eb0e32da95c3f41874765f0c
Mathias Dahlström <mathias.dahlstrom@gmail.com>
2025-03-03 10:11:34 +0100

    fix: adding service annotations to traefik

--- lib/aks/traefik-gateway.d.ts
+++ lib/aks/traefik-gateway.d.ts
@@ -1,9 +1,12 @@
 import * as k8s from '@pulumi/kubernetes';
+import * as pulumi from '@pulumi/pulumi';
 import * as kubernetes from '@pulumi/kubernetes';
 export declare let chart: k8s.helm.v3.Chart;
 export type TraefikGatewayArgs = {
   version?: string;
+  traefikVersion?: string;
   provider: kubernetes.Provider;
+  loadbalancerSubnetName: string | pulumi.Input<string>;
 };
 export declare function setup(input: TraefikGatewayArgs): void;
 //# sourceMappingURL=traefik-gateway.d.ts.map
--- src/aks/traefik-gateway.ts
+++ src/aks/traefik-gateway.ts
@@ -5,9 +5,14 @@ import * as kubernetes from '@pulumi/kubernetes';
 
 export let chart: k8s.helm.v3.Chart;
 
+const traefikVersion = '3.3.4';
+const traefikHelmVersion = '34.4.0';
+
 export type TraefikGatewayArgs = {
   version?: string;
+  traefikVersion?: string;
   provider: kubernetes.Provider;
+  loadbalancerSubnetName: string | pulumi.Input<string>;
 };
 export function setup(input: TraefikGatewayArgs): void {
   const ns = new kubernetes.core.v1.Namespace(
@@ -24,15 +29,43 @@ export function setup(input: TraefikGatewayArgs): void {
     {
       repo: 'traefik',
       chart: 'traefik',
-      version: input.version ? input.version : '34.4.0',
+      version: input.version ? input.version : traefikHelmVersion,
       namespace: ns.metadata.name,
       fetchOpts: {
         repo: 'https://traefik.github.io/charts',
       },
       values: {
-        experimental: {
+        deployment: {
+          kind: 'DaemonSet',
+        },
+        service: {
+          enabled: true,
+          type: 'LoadBalancer',
+          annotations: {
+            'service.beta.kubernetes.io/azure-load-balancer-internal': 'true',
+            'service.beta.kubernetes.io/azure-load-balancer-internal-subnet':
+              input.loadbalancerSubnetName,
+          },
+        },
+        image: {
+          tag: input.traefikVersion ? input.traefikVersion : traefikVersion,
+        },
+        gateway: {
+          enabled: false,
+        },
+        providers: {
+          kubernetesIngress: {
+            enabled: false,
+          },
+          kubernetesCRD: {
+            enabled: false,
+          },
+          file: {
+            enabled: false,
+          },
           kubernetesGateway: {
             enabled: true,
+            experimentalChannel: true,
           },
         },
       },
